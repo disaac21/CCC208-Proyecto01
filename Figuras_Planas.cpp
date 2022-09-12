95c1c654840590ca8a64d68f16f1bb68a6e4cb20
IanPotato3 <105087122+IanPotato3@users.noreply.github.com>
2022-09-12 15:28:05 -0400

    Cambio en Funciones.h y lógica de interpretación
    Todo se encuentra documentado en comentarios, cualquier duda contactar

--- Figuras_Planas.cpp
+++ Figuras_Planas.cpp
@@ -1,13 +1,16 @@
-//#include <iostream>
-#include <string>
-#include <vector>
-#include "Funciones.h"
-//using namespace std;
+#include "Funciones.h" //include de archivo header con todas las funciones, Funciones.h no es una clase, es un archivo con funciones adentro
 
 int main(int argc, char *argv[]) {
-    vector<string> parametros;
-    for (int i = 0; i < argc; i++)
-        parametros.push_back(argv[i]);
-    Funciones f; f.SwitchFiguras(parametros);
-    getchar();
+    SwitchFiguras(argc, argv); //SwitchFiguras es una funcion de Funciones.h, agarra como parametros a argc(# de argumentos) y argv(array de strings)
 }
+
+/*
+Anteriormente Funciones.h trabajaba como objeto,
+ahora que simplemente es un archivo con funciones,
+solo hace falta incluirlo y seremos capaces de utilizar libremente sus funciones.
+
+Anteriormente teniamos tambien un vector con los parametros,
+esto lo descartamos arrastrando directamente su array de argumentos.
+Esto cambia la forma en que se manejan los datos(es posible notarlo al seguir viendo el programa),
+sin embargo, la logica del programa es la misma.
+*/
--- Funciones.h
+++ Funciones.h
@@ -1,18 +1,197 @@
-#pragma once
 #include <iostream>
 #include <string>
 #include <vector>
+#include <fstream>
+#include <windows.h>
+
 using namespace std;
 
-class Funciones {
+//Declaracion -----------------------------------------------------
+//void mainPrima(); Idea de funcion en espera...
+void SwitchFiguras(int argc, char* argv[]);
+void MensajeError();
+int CargarArchivo(int argc, char* argv[]);
 
-    private:
-        string ElegirFigura;
+//Definicion ------------------------------------------------------
+void SwitchFiguras(int argc, char* argv[]) { //argc y argv siguen la misma logica mencionada anteriormente, argc(# de argumentos) y argv(array de strings)
+    SetConsoleCP(CP_UTF8);
+    SetConsoleOutputCP(CP_UTF8);
 
-    public:
-        Funciones();
-        void SwitchFiguras(vector<string>);
-        void MensajeError();
-        void CargarArchivo(vector<string>);
+    if (argc <= 2) {
+        MensajeError();
+    }else {
+        cout << endl;
+        CargarArchivo(argc, argv);
+    }
+}
 
+/*
+COMENTARIO A TENER EN CUENTA - EXTRAIDO DEL PDF
+Por cada figura se debe crear una clase con su respectivo .h y .cpp, misma que se
+instanciera en alguna función del archivo de funciones.
+
+La implementacion de la gráfica debe estar en un metodo llamado Dibujar en la clase,
+
+TAMBIÉN: Revisar Referencias
+*/
+
+int CargarArchivo(int argc, char* argv[]) {
+    string figura = argv[1]; //Pasamos los argumentos a string para poder comparar con const strings con facilidad
+
+    if (figura == "triangulo") {
+        if (argc == 6) {
+            ifstream ArchivoTriangulo("Triangulo.txt", ios::in);
+
+            if (!ArchivoTriangulo) {
+                cerr << "No se pudo abrir el archivo" << endl;
+                exit(EXIT_FAILURE);
+            } //Se verifica si el archivo se abrio correctamente
+
+            string line;
+            while (getline(ArchivoTriangulo, line))
+                cout << line << endl; //Se lee el archivo linea por linea
+        }
+        else {
+            MensajeError();
+        }
+    }
+
+    if (figura == "paralelogramo") {
+        if (argc == 5) {
+            ifstream ArchivoParalelogramo("Paralelogramo.txt", ios::in);
+
+            if (!ArchivoParalelogramo) {
+                cerr << "No se pudo abrir el archivo" << endl;
+                exit(EXIT_FAILURE);
+            }
+
+            string line;
+            while (getline(ArchivoParalelogramo, line))
+                cout << line << endl;
+        }
+        else {
+            MensajeError();
+        }
+    }
+
+    if (figura == "rectangulo") {
+        if (argc == 4) {
+            ifstream ArchivoRectangulo("Rectangulo.txt", ios::in);
+
+            if (!ArchivoRectangulo) {
+                cerr << "No se pudo abrir el archivo" << endl;
+                exit(EXIT_FAILURE);
+            }
+
+            string line;
+            while (getline(ArchivoRectangulo, line))
+                cout << line << endl;
+        }
+        else {
+            MensajeError();
+        }
+    }
+
+    if (figura == "cuadrado") {
+        if (argc == 3) {
+            ifstream ArchivoCuadrado("Cuadrado.txt", ios::in);
+
+            if (!ArchivoCuadrado) {
+                cerr << "No se pudo abrir el archivo" << endl;
+                exit(EXIT_FAILURE);
+            }
+
+            string line;
+            while (getline(ArchivoCuadrado, line))
+                cout << line << endl;
+        }
+        else {
+            MensajeError();
+        }
+    }
+
+    if (figura == "rombo") {
+        if (argc == 5) {
+            ifstream ArchivoRombo("Rombo.txt", ios::in);
+
+            if (!ArchivoRombo) {
+                cerr << "No se pudo abrir el archivo" << endl;
+                exit(EXIT_FAILURE);
+            }
+
+            string line;
+            while (getline(ArchivoRombo, line))
+                cout << line << endl;
+        }
+        else {
+            MensajeError();
+        }
+    }
+
+    if (figura == "cometa") {
+        if (argc == 6) {
+            ifstream ArchivoCometa("Cometa.txt", ios::in);
+
+            if (!ArchivoCometa) {
+                cerr << "No se pudo abrir el archivo" << endl;
+                exit(EXIT_FAILURE);
+            }
+
+            string line;
+            while (getline(ArchivoCometa, line))
+                cout << line << endl;
+        }
+        else {
+            MensajeError();
+        }
+    }
+
+    if (figura == "trapecio") {
+        if (argc == 7) {
+            ifstream ArchivoTrapecio("Trapecio.txt", ios::in);
+
+            if (!ArchivoTrapecio) {
+                cerr << "No se pudo abrir el archivo" << endl;
+                exit(EXIT_FAILURE);
+            }
+
+            string line;
+            while (getline(ArchivoTrapecio, line))
+                cout << line << endl;
+        }
+        else {
+            MensajeError();
+        }
+    }
+
+    if (figura == "circulo") {
+        if (argc == 3) {
+            ifstream ArchivoCirculo("Circulo.txt", ios::in);
+
+            if (!ArchivoCirculo) {
+                cerr << "No se pudo abrir el archivo" << endl;
+                exit(EXIT_FAILURE);
+            }
+
+            string line;
+            while (getline(ArchivoCirculo, line))
+                cout << line << endl;
+        }
+        else {
+            MensajeError();
+        }
+    }
 };
+
+void MensajeError() {
+    ifstream ArchivoNoParametros("NoParametros.txt", ios::in);
+
+    if (!ArchivoNoParametros) {
+        cerr << "No se pudo abrir el archivo" << endl;
+        exit(EXIT_FAILURE);
+    } //Se verifica si se abrio correctamente el archivo
+
+    string line;
+    while (getline(ArchivoNoParametros, line))
+        cout << line << endl;
+}
